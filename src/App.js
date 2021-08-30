1dc7dd13062d55ce9c5f995fa7a885c9300943f6
trey10hughes <trey10hughes@gmail.com>
2021-08-30 17:25:25 -0500

    set up grid for base layout of app

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
       <header className="App-header">
         AVB Coding Test: Trey Hughes
       </header>
-      <div class="inline-grid grid-cols-2 gap-4">
-        <div className="contacts object-left">
+      <div class="main-grid inline-grid grid-cols-4 gap-4">
+        <div className="contacts object-left col-span-1 col-start-1">
           {/* this is the div that will hold the list of contacts & the button to add a new one */}
           contacts
         </div>
-        <div className="contactDetails object-right">
+        <div className="contactDetails object-right col-span-3 col-start-2">
           {/* this is the div that will hold more info about a selected contact */}
           {/* this will allow customers to add and delete emails from a contact */}
           contact details
