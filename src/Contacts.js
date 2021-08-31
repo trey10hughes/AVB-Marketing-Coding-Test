fb2be5405c5dc4da3338ececee4e5ec2a67022ad
trey10hughes <trey10hughes@gmail.com>
2021-08-31 17:29:04 -0500

    input for adding a new contact built, need to work on positioning and conditional rendering

--- src/Contacts.js
+++ src/Contacts.js
@@ -65,6 +65,21 @@ class Contacts extends React.Component {
 
                 {/* Input form to add new contact */}
                 
+                {/* figure out how to get this to render without moving the above portion up */}
+
+                {/* <form className="w-full max-w-sm gric gric-cols-1">
+                    <div className="md:flex md:items-center mb-6">
+                        <div className="md:w-1/3">
+                            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-first-name">
+                                First Name
+                            </label>
+                        </div>
+                        <div class="md:w-2/3">
+                            <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe"></input>
+                        </div>
+                    </div>
+                </form> */}
+                
                 <ContactList parentCallback={this.handleCallback}/>
             </div>
         );
