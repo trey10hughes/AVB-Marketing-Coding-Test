3a61383130b761ba5ab92b650797c755468553b7
trey10hughes <trey10hughes@gmail.com>
2021-09-02 16:12:54 -0500

    set up grid for contactDetails section

--- src/ContactDetails.js
+++ src/ContactDetails.js
@@ -24,8 +24,33 @@ class ContactDetails extends React.Component {
         }
         
         return(
-            <div>
-                {contactFirstName}
+            // requirements:
+            // display first name and last name of contact in input boxes at the top
+            // display list of emails underneath first name input box
+            // when hovering an email in the list, a delete button will appear
+            // an add email button appears at the bottom of the list
+            // delete button at the bottom left
+            // cancel and save buttons at the bottom right
+
+            // develop the UI first, then go back and add the functionality to edit and delete
+
+            <div className="flex grid gap-2 grid-cols-4 grid-rows-4 contactsDiv">
+                <div className="bg-blue-100">{contactFirstName}</div>
+                <div className="bg-blue-200">{contactFirstName}</div>
+                <div className="bg-blue-100">{contactFirstName}</div>
+                <div className="bg-blue-200">{contactFirstName}</div>
+                <div className="bg-blue-100">{contactFirstName}</div>
+                <div className="bg-blue-200">{contactFirstName}</div>
+                <div className="bg-blue-100">{contactFirstName}</div>
+                <div className="bg-blue-200">{contactFirstName}</div>
+                <div className="bg-blue-100">{contactFirstName}</div>
+                <div className="bg-blue-200">{contactFirstName}</div>
+                <div className="bg-blue-100">{contactFirstName}</div>
+                <div className="bg-blue-200">{contactFirstName}</div>
+                <div className="bg-blue-100">{contactFirstName}</div>
+                <div className="bg-blue-200">{contactFirstName}</div>
+                <div className="bg-blue-100">{contactFirstName}</div>
+                <div className="bg-blue-200">{contactFirstName}</div>
             </div>
         )
     }
