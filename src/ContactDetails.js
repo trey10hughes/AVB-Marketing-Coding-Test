ef0073d3e064500229c6ba18851e260df9ec141c
Trey <trey10hughes@gmail.com>
2021-09-02 20:05:03 -0500

    progress on edit/delete

--- src/ContactDetails.js
+++ src/ContactDetails.js
@@ -38,7 +38,20 @@ class ContactDetails extends React.Component {
                 <div className="row-span-4"></div>
 
                 {/* first name input */}
-                <div className="bg-blue-100">{contactFirstName}</div>
+                <div className="bg-blue-100">
+                    <form className="w-full max-w-sm grid grid-cols-1 static px-4">
+                        <div className="md:flex md:items-center mb-6">
+                            <div className="">
+                                <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-first-name">
+                                    First Name
+                                </label>
+                            </div>
+                            <div className="">
+                                <input name="firstName" onChange={this.handleChange} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400" id="inline-full-name" type="text"></input>
+                            </div>
+                        </div>
+                    </form>
+                </div>
                 {/* email list */}
                 <div className="bg-blue-100 row-span-2">{contactFirstName}</div>
                 {/* delete button */}
